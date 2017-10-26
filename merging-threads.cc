182bd10193314f9320d457427662969cc1e17761
Don Vu <don.vu1212@gmail.com>
2017-10-25 21:58:44 -0700

    Insert file numbers to global array

--- merging-threads.cc
+++ merging-threads.cc
@@ -8,13 +8,31 @@
 //  into the sorted list of the original numbers.
 
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
+
+//  Global array threads access
+vector<int> initiallist;
 
 int main (int argc, char *argv[]) {
   if (argc != 2) {
     cerr << "Usage of the program is: " << argv[0]
       << " <file name of unsorted numbers>\n";
   }
+
+  ifstream numlist (argv[1]);
+  if (!numlist.is_open()) {
+    cerr << "Unable to open file\n";
+    return 1;
+  }
+  
+  int getnum;
+  while (numlist >> getnum) {
+    initiallist.push_back(getnum); 
+  }
+
+  numlist.close();
 
   return 0;
 }
